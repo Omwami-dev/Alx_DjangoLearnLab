d385946c40932731ed115edf4f4ae8e8e92fd09b
Omwami-dev <barasaowabarasa@gmail.com>
2025-07-20 20:53:19 +0300

    updated file query

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -5,7 +5,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 def books_by_author(author_name):
     try:
         author = Author.objects.get(name=author_name)
-        books = author.books.all()
+        books = objects.filter(author=author)
         print(f"Books by {author.name}: {[book.title for book in books]}")
     except Author.DoesNotExist:
         print("Author not found.")
