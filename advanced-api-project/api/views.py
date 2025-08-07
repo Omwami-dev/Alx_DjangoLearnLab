34e63b62988a1e280a020e50db0c25afe985173b
Omwami-dev <barasaowabarasa@gmail.com>
2025-08-07 22:58:46 +0300

    update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,20 +3,22 @@ from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
 
-class BookListView(generics.ListAPIView):
+class BookListView(generics.ListAPIView): # Purpose: Returns a list of all book records.
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.AllowAny] # Permissions: Open to all users (unauthenticated allowed).
 
-class BookDetailView(generics.RetrieveAPIView):
+class BookDetailView(generics.RetrieveAPIView): # Purpose: Retrieve a single book by ID.
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.AllowAny] # Permissions: Open to all users.
 
-class BookCreateView(generics.CreateAPIView):
+
+class BookCreateView(generics.CreateAPIView): #  Purpose: Allow authenticated users to create a new book.
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated] # Permissions: Requires login.
+
 
 class BookUpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
