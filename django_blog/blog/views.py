716a9d00f8efa93e936262082dcebad2252c0db8
Omwami-dev <barasaowabarasa@gmail.com>
2025-08-17 13:03:07 +0300

    update blog app

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -4,3 +4,35 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Hello, this is the Blog homepage!")
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import UserRegistrationForm, UserUpdateForm, ProfileUpdateForm
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('profile')
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Account created. You can log in now.")
+            return redirect('login')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    if request.method == "POST":
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, "Profile updated successfully.")
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+    return render(request, 'blog/profile.html', {'u_form': u_form, 'p_form': p_form})
