806437d12328d0cb10640f1f4afeceadf8dde654
Omwami-dev <barasaowabarasa@gmail.com>
2025-08-17 11:17:36 +0300

    update models

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-
-from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -11,3 +11,21 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+def avatar_upload_path(instance, filename):
+    return f'avatars/user_{instance.user.id}/{filename}'
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    bio = models.TextField(blank=True)
+    avatar = models.ImageField(upload_to=avatar_upload_path, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    else:
+        instance.profile.save()
