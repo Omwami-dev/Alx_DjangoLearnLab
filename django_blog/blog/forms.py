7c71deda65c1b0f42f9702ffa8c910b223c5714f
Omwami-dev <barasaowabarasa@gmail.com>
2025-08-23 16:08:18 +0300

    update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,13 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
 from .models import Profile
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
+
 
 User = get_user_model()
 
