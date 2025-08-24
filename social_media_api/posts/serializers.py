4018b651067e3ed76e9418839064e2dc124692c4
Omwami-dev <barasaowabarasa@gmail.com>
2025-08-24 13:03:35 +0300

    add posts serializers

---
+++ social_media_api/posts/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from .models import Post, Comment
+
+User = get_user_model()
+
+class CommentSerializer(serializers.ModelSerializer):
+    author_username = serializers.ReadOnlyField(source='author.username')
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'post', 'author', 'author_username', 'content', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'author', 'author_username', 'created_at', 'updated_at']
+
+class PostSerializer(serializers.ModelSerializer):
+    author_username = serializers.ReadOnlyField(source='author.username')
+    comments = CommentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ['id', 'author', 'author_username', 'title', 'content', 'created_at', 'updated_at', 'comments']
+        read_only_fields = ['id', 'author', 'author_username', 'created_at', 'updated_at', 'comments']
+
+    def create(self, validated_data):
+        # author will be set in the view (from request.user), but keep this hook if needed
+        return Post.objects.create(**validated_data)
