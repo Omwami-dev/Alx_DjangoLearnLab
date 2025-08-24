534d6725fe049ba4e69325dda69361f8d3925220
Omwami-dev <barasaowabarasa@gmail.com>
2025-08-24 13:23:06 +0300

    update serializers

--- social_media_api/posts/serializers.py
+++ social_media_api/posts/serializers.py
@@ -24,3 +24,21 @@ class PostSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         # author will be set in the view (from request.user), but keep this hook if needed
         return Post.objects.create(**validated_data)
+from rest_framework import serializers
+from .models import Post, Comment
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'post', 'author', 'content', 'created_at', 'updated_at']
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+    comments = CommentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'content', 'author', 'created_at', 'updated_at', 'comments']
